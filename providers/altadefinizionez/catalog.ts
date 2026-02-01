2bb3ddd9333d81f2889dc74d57f3e56eb4e9b705
Nokitomo <aronnegiuseppe@gmail.com>
2026-02-01 17:11:57 +0100

    Add altadefinizionez catalog

---
+++ providers/altadefinizionez/catalog.ts
@@ -0,0 +1,53 @@
+export const catalog = [
+  {
+    title: "Film",
+    filter: "film?tipo=1",
+  },
+  {
+    title: "Serie TV",
+    filter: "film?tipo=2",
+  },
+  {
+    title: "Archivio",
+    filter: "catalog/all",
+  },
+  {
+    title: "Commedia",
+    filter: "commedia",
+  },
+  {
+    title: "Thriller",
+    filter: "thriller",
+  },
+  {
+    title: "Fantascienza",
+    filter: "fantascienza",
+  },
+];
+
+export const genres = [
+  {
+    title: "Azione",
+    filter: "azione",
+  },
+  {
+    title: "Animazione",
+    filter: "animazione",
+  },
+  {
+    title: "Commedia",
+    filter: "commedia",
+  },
+  {
+    title: "Drammatico",
+    filter: "drammatico",
+  },
+  {
+    title: "Fantascienza",
+    filter: "fantascienza",
+  },
+  {
+    title: "Thriller",
+    filter: "thriller",
+  },
+];
