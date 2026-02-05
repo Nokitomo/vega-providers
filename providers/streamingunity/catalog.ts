12ed1139f658a707dcd4b05f5a3fcf081e86f108
Nokitomo <aronnegiuseppe@gmail.com>
2026-02-05 16:50:02 +0100

    Add streamingunity catalog

---
+++ providers/streamingunity/catalog.ts
@@ -0,0 +1,34 @@
+export const catalog = [
+  {
+    title: "Trending Titles",
+    titleKey: "Trending Titles",
+    filter: "browse/trending",
+  },
+  {
+    title: "Recently Added",
+    titleKey: "Recently Added",
+    filter: "browse/latest",
+  },
+  {
+    title: "Top 10 Titles Today",
+    titleKey: "Top 10 Titles Today",
+    filter: "browse/top10",
+  },
+  {
+    title: "Movie",
+    titleKey: "Movie",
+    filter: "archive?type=movie",
+  },
+  {
+    title: "TV Show",
+    titleKey: "TV Show",
+    filter: "archive?type=tv",
+  },
+  {
+    title: "Archive",
+    titleKey: "Archive",
+    filter: "archive",
+  },
+];
+
+export const genres = [];
